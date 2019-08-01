b7282cfda71f50353ee3e354c420eb142a1600b0
Jean-Philippe Côté <null@djip.co>
2019-08-01 12:42:33 -0400

    Deprecate once() in favour of addListener() with option

--- src/djipevents.js
+++ src/djipevents.js
@@ -126,30 +126,12 @@ export class EventEmitter {
   }
 
   /**
-   * Adds a one-time listener for the specified event. It returns the `Listener` object that was
-   * created and attached to the event.
-   *
-   * To attach a global listener that will be triggered for any events, use `EventEmitter.ANY_EVENT`
-   * as the first parameter.
-   *
-   * @param {String|EventEmitter.ANY_EVENT} event The event to listen to
-   * @param {EventEmitter~callback} callback The callback function to execute when the event occurs
-   * @param {Object} [options={}]
-   * @param {Object} [options.context=this] The context to invoke the callback function in.
-   * @param {boolean} [options.prepend=false] Whether the listener should be added at the beginning
-   * of the listeners array
-   * @param {number} [options.duration=Infinity] The number of milliseconds before the listener
-   * automatically expires.
-   * @param {*} [options.data] Arbitrary data to pass on to the callback function upon execution
-   *
-   * @returns {Listener}
-   *
-   * @throws {TypeError} The `event` parameter must be a string or EventEmitter.ANY_EVENT.
-   * @throws {TypeError} The `callback` parameter must be a function.
+   * @private
+   * @deprecated
    */
   once(event, callback, options = {}) {
     options.remaining = 1;
-    return this.on(event, callback, options);
+    return this.addListener(event, callback, options);
   }
 
   /**
