156936a5aae5a06af5c1bd52da828d09ca6b6698
Jean-Philippe Côté <jp@djip.co>
2021-11-10 21:56:37 -0500

    Omit unnecessary double-quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
 
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-    "mocha": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true
   },
 
-  "parser": "babel-eslint",
+  // parser: "@babel/eslint-parser",
 
-  "parserOptions": {
-    "ecmaVersion": 8,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: "module"
   },
 
-  "extends": ["eslint:recommended", "prettier"],
+  extends: ["eslint:recommended", "prettier"],
 
-  "rules": {
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+  rules: {
+    indent: ["error", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
-    "max-len": ["error", { "code": 100 }],
-    "no-console": ["error", { "allow": ["info", "warn", "error"] }],
-    "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
-    "no-trailing-spaces": ["error", { "skipBlankLines": true, "ignoreComments": true }],
+    "max-len": ["error", { code: 100 }],
+    "no-console": ["error", { allow: ["info", "warn", "error"] }],
+    "no-multi-spaces": ["error", { ignoreEOLComments: true }],
+    "no-trailing-spaces": ["error", { skipBlankLines: true, ignoreComments: true }],
     "no-underscore-dangle": "off",
     "quote-props": ["error", "as-needed"],
-    "quotes": ["error", "double", {"avoidEscape":  true, "allowTemplateLiterals": true}],
-    "semi": ["error", "always"]
+    quotes: ["error", "double", {avoidEscape:  true, allowTemplateLiterals: true}],
+    semi: ["error", "always"]
   }
 
 };
