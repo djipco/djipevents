e3b3afe995e4dd405fad4a27005e0b576b3c78c7
Jean-Philippe Côté <jp@djip.co>
2021-11-10 21:59:31 -0500

    Move lint task to package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 const gulp = require("gulp");
-const eslint = require("gulp-eslint");
 const ghPages = require("gulp-gh-pages");
 const jsdoc = require("gulp-jsdoc3");
 const spawn = require("child_process").spawn;
@@ -19,18 +18,11 @@ function publishDoc() {
     .pipe(ghPages());
 }
 
-function lint() {
-  return gulp.src(["src/*.js"])
-    .pipe(eslint())
-    .pipe(eslint.failAfterError());
-}
-
 function releaseToNpm(cb) {
   spawn("npm", ["publish"], { stdio: "inherit" }).on("close", cb);
 }
 
 const doc = gulp.series(generateDoc, publishDoc);
 
-exports.lint = lint;
 exports.doc = doc;
 exports.release = releaseToNpm;
